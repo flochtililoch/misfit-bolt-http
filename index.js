e2a46928d68ca11859c45658fc796b065e0aeb81
flochtililoch <flochtililoch@gmail.com>
2016-06-12 14:38:03 -0700

    Update to latest misfit-bolt. Make bolts routes dynamic, and rely on Bolt.get to retrieve bolt instances.

--- index.js
+++ index.js
@@ -2,26 +2,67 @@
 
 'use strict';
 
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    Bolt = require('misfit-bolt'),
-    debug = require('debug')(require('./package').name),
-    actions = require('./lib/actions'),
-    argv = require('minimist')(process.argv.slice(2));
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      Bolt = require('misfit-bolt'),
+      debug = require('debug')(require('./package').name),
+      argv = require('minimist')(process.argv.slice(2));
 
-var app = express(),
-    port = parseInt(argv.p, 10) || 3000,
-    bolts = [];
+const app = express(),
+      port = parseInt(argv.p, 10) || 3000,
+      actions = ['state', 'hue', 'saturation', 'brightness'],
+      ucfirst = (name) => {
+        return name.charAt(0).toUpperCase() + name.slice(1);
+      };
 
 app.use(bodyParser.json());
 app.listen(port, () => { console.log(`listening on port ${port}`); });
 
 app.get('/', (req, res) => {
+  const bolts = Bolt.bolts.map((bolt) => {
+    return {
+      id: bolt.id,
+      connected: bolt.connected
+    };
+  });
   res.send({bolts});
 });
 
-Bolt.discover(function(bolt) {
-  debug(`New bolt discovered: ${bolt.id}`);
-  bolts.push(bolt.id);
-  actions(app, bolt);
+actions.forEach((name) => {
+
+  app.get(`/:id/${name}`, (req, res) => {
+    const method = `get${ucfirst(name)}`,
+          bolt = Bolt.get(req.params.id);
+    if (bolt) {
+      debug(`${method} called for ${req.params.id}`);
+      bolt.connect(() => {
+        bolt[method]((err, value) => {
+          debug(`${method} returned value: ${value}`);
+          var body = {};
+          body[name] = value;
+          res.send(body);
+        });
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+
+  app.put(`/:id/${name}`, (req, res) => {
+    const method = `set${ucfirst(name)}`,
+          bolt = Bolt.get(req.params.id);
+    if (bolt) {
+      debug(`${method} called for ${req.params.id}`);
+      bolt.connect(() => {
+        bolt[method](req.body ? req.body[name] : undefined, (err) => {
+          res.end();
+        });
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+
 });
+
+Bolt.discover();
