1ddd8c98f617f15f9b65158df9adde7719d663af
flochtililoch <flochtililoch@gmail.com>
2016-06-12 19:23:52 -0700

    Add error handling.

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 #! /usr/bin/env node
 
-'use strict';
+"use strict";
 
 const express = require('express'),
       bodyParser = require('body-parser'),
       Bolt = require('misfit-bolt'),
       debug = require('debug')(require('./package').name),
-      argv = require('minimist')(process.argv.slice(2));
+      argv = require('minimist')(process.argv.slice(2)),
+      util = require('./lib/util'),
+      ucfirst = util.ucfirst,
+      assert = util.assert,
+      errorHandler = util.errorHandler,
+      responseHandler = util.responseHandler,
+      errors = require('./lib/errors'),
+      NotFound = errors.NotFound,
+      BadRequest = errors.BadRequest,
+      ServerError = errors.ServerError;
 
 const app = express(),
       port = parseInt(argv.p, 10) || 3000,
-      actions = ['state', 'hue', 'saturation', 'brightness'],
-      ucfirst = (name) => {
-        return name.charAt(0).toUpperCase() + name.slice(1);
-      };
+      actions = ['state', 'hue', 'saturation', 'brightness'];
 
 app.use(bodyParser.json());
 app.listen(port, () => { console.log(`listening on port ${port}`); });
@@ -31,36 +37,48 @@ app.get('/', (req, res) => {
 actions.forEach((name) => {
 
   app.get(`/:id/${name}`, (req, res) => {
-    const method = `get${ucfirst(name)}`,
-          bolt = Bolt.get(req.params.id);
-    if (bolt) {
-      debug(`${method} called for ${req.params.id}`);
+
+    try {
+      const method = `get${ucfirst(name)}`,
+          id = req.params.id,
+          bolt = Bolt.get(id);
+      assert(bolt, NotFound, `cannot find Bolt with id ${id}`);
+
+      debug(`${method} called for ${id}`);
       bolt.connect(() => {
         bolt[method]((err, value) => {
           debug(`${method} returned value: ${value}`);
-          var body = {};
-          body[name] = value;
-          res.send(body);
+          responseHandler(res, name, value);
         });
       });
-    } else {
-      res.status(404).end();
+    } catch (err) {
+      debug(`error ${err}`);
+      errorHandler(err, res);
     }
+
   });
 
   app.put(`/:id/${name}`, (req, res) => {
-    const method = `set${ucfirst(name)}`,
-          bolt = Bolt.get(req.params.id);
-    if (bolt) {
-      debug(`${method} called for ${req.params.id}`);
+
+    try {
+      const method = `set${ucfirst(name)}`,
+            id = req.params.id,
+            bolt = Bolt.get(id);
+      assert(bolt, NotFound, `cannot find Bolt with id ${id}`);
+      assert(req.body && req.body[name], BadRequest, `missing value for ${name}`);
+
+      const value = req.body[name];
+      debug(`${method} called for ${id} with ${value}`);
       bolt.connect(() => {
-        bolt[method](req.body ? req.body[name] : undefined, (err) => {
-          res.end();
+        bolt[method](value, (err) => {
+          responseHandler(res, name, value);
         });
       });
-    } else {
-      res.status(404).end();
+    } catch (err) {
+      debug(`error ${err.status}, ${err.message}`);
+      errorHandler(err, res);
     }
+
   });
 
 });
